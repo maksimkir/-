66bce758a70abd240b63e4a437c755c8c57709c0
Max <kirilukm157@gmail.com>
2025-09-25 23:07:26 +0300

    add update for lab_1 , 1.1 and 1.3

---
+++ lab_1/1.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Write your radius R:");
+        Console.Write("R = ");
+        int R = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Write your point coordinates:");
+        Console.Write("x = ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.Write("y = ");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        string result = Popav(x, y, R);
+        Console.WriteLine(result);
+    }
+
+    static string Popav(int x, int y, int R)
+    {
+        // Перша область
+        if (x >= 0 && y >= 0)
+        {
+            double d = x * x + y * y;
+            if (Math.Abs(d - R * R) < 1e-9) return "At the boundary";
+            if (d < R * R) return "Yes";
+        }
+        // Друга область 
+        else if (x <= 0 && y <= 0)
+        {
+            double d = x * x + (y + R) * (y + R);
+            if (Math.Abs(d - R * R) < 1e-9) return "At the boundary";
+            if (d < R * R) return "Yes";
+        }
+
+        return "No";
+    }
+    
+}
