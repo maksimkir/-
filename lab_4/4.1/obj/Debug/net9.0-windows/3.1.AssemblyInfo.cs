fc5378bc524dcac368d2a75d1751aab63bbe4011
Max <kirilukm157@gmail.com>
2025-10-25 12:22:56 +0300

    add update

--- lab_4/4.1/obj/Debug/net9.0-windows/3.1.AssemblyInfo.cs
+++ lab_4/4.1/obj/Debug/net9.0-windows/3.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e798bd2da3f287cc83bc83cb7aa444d334486d37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+532dff50ecc31a5e27d037013dc66cef317c2166")]
 [assembly: System.Reflection.AssemblyProductAttribute("3.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- lab_5/5.3/obj/Debug/net9.0-windows/5.1.AssemblyInfo.cs
+++ lab_5/5.3/obj/Debug/net9.0-windows/5.1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("4.3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("5.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+532dff50ecc31a5e27d037013dc66cef317c2166")]
-[assembly: System.Reflection.AssemblyProductAttribute("4.3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("4.3")]
+[assembly: System.Reflection.AssemblyProductAttribute("5.1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("5.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
