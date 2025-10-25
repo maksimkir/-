d53384a3cb6cb34c7882b2b27d4b0ab8512c9d75
Max <kirilukm157@gmail.com>
2025-10-25 10:50:43 +0300

    add update

--- lab_4/4.3/obj/Debug/net9.0-windows/4.3.AssemblyInfo.cs
+++ lab_4/4.3/obj/Debug/net9.0-windows/4.3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("3.1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("4.3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e798bd2da3f287cc83bc83cb7aa444d334486d37")]
-[assembly: System.Reflection.AssemblyProductAttribute("3.1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("3.1")]
+[assembly: System.Reflection.AssemblyProductAttribute("4.3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("4.3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
