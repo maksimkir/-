4d97250b207a6d9a2ea4dd7b60b29c98f29e13f8
Max <kirilukm157@gmail.com>
2025-10-27 01:10:13 +0200

    add update

--- lab_6/6.3/obj/Debug/net9.0-windows/6.2.AssemblyInfo.cs
+++ lab_6/6.3/obj/Debug/net9.0-windows/6.2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6.2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9794b3eb862e32b4684a27c56b0524305c9e8d98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb3814e75161f72cc27a27fc5624987aa457b251")]
 [assembly: System.Reflection.AssemblyProductAttribute("6.2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6.2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
